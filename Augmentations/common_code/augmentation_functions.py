125371a3bd71c04a3f7436db64daa0aa94578715
Tomas Mūžas <muzas.tomas@gmail.com>
2023-01-17 19:38:34 +0000

    Add targeted noise

--- Augmentations/common_code/augmentation_functions.py
+++ Augmentations/common_code/augmentation_functions.py
@@ -1,4 +1,17 @@
 import tensorflow as tf
+import numpy as np
+from astropy.visualization import PercentileInterval
+from astropy.modeling.models import Sersic2D
+
+x,y = np.meshgrid(np.arange(128), np.arange(128))
+
+mod = Sersic2D(amplitude = 3, r_eff = 5, n=1, x_0=64, y_0=64,
+               ellip=0, theta=1)
+img = mod(x, y)
+log_img = np.log10(img)
+center_noise_profile_2d = PercentileInterval(90)(log_img)
+center_noise_profile = np.repeat(center_noise_profile_2d[:,:,None], 3, axis=2)
+outside_noise_profile = 1 - center_noise_profile
 
 @tf.function
 def random_invert_horizontally(x, seed):
@@ -35,6 +48,29 @@ def random_noise(x, seed):
   if  tf.random.uniform([], seed = seed) < 0.5:
     noise = tf.random.uniform(shape=tf.shape(x), minval= -0.2, maxval= 0.2, dtype=tf.float32)
     x = tf.add(x, noise)
+    x = tf.clip_by_value(x, clip_value_min=0, clip_value_max=1)
+  else:
+    x
+  return x
+
+
+@tf.function
+def targeted_center_noise(x, seed):
+  if  tf.random.uniform([], seed = seed) < 0.5:
+    noise = tf.random.uniform(shape=tf.shape(x), minval= -0.2, maxval= 0.2, dtype=tf.float32)
+    center_noise = tf.math.multiply(noise, center_noise_profile)
+    x = tf.add(x, center_noise)
+    x = tf.clip_by_value(x, clip_value_min=0, clip_value_max=1)
+  else:
+    x
+  return x
+
+@tf.function
+def targeted_outside_noise(x, seed):
+  if  tf.random.uniform([], seed = seed) < 0.5:
+    noise = tf.random.uniform(shape=tf.shape(x), minval= -0.2, maxval= 0.2, dtype=tf.float32)
+    outside_noise = tf.math.multiply(noise, outside_noise_profile)
+    x = tf.add(x, outside_noise)
     x = tf.clip_by_value(x, clip_value_min=0, clip_value_max=1)
   else:
     x
--- Augmentations/common_code/get_dataset.py
+++ Augmentations/common_code/get_dataset.py
@@ -38,6 +38,10 @@ def get_dataset(training_config, path, batch_size, seed, augment, shuffle, drop_
         dataset = dataset.map(lambda x,y : (random_rotate(x, seed), y), num_parallel_calls=AUTO)
       if training_config["AUGMENTATIONS_RANDOM_NOISE"]:
         dataset = dataset.map(lambda x,y : (random_noise(x, seed), y), num_parallel_calls=AUTO)
+      if training_config["AUGMENTATIONS_CENTER_NOISE"]:
+        dataset = dataset.map(lambda x,y : (targeted_center_noise(x, seed), y), num_parallel_calls=AUTO)
+      if training_config["AUGMENTATIONS_OUTSIDE_NOISE"]:
+        dataset = dataset.map(lambda x,y : (targeted_outside_noise(x, seed), y), num_parallel_calls=AUTO)
 
   dataset = dataset.batch(batch_size, drop_remainder=drop_remainder).prefetch(200 if training_config["TPU"] else 3)
 
