ad740d04f077dc61f7fdb48748bb6a69044c3ae2
Tomas Mūžas <muzas.tomas@gmail.com>
2023-01-26 19:35:18 +0000

    Update some notebooks

--- Augmentations/common_code/models.py
+++ Augmentations/common_code/models.py
@@ -53,21 +53,12 @@ def Dieleman(training_config):
 def SimpleModel(training_config):
   model = Sequential(name="SimpleModel")
   model.add(Input(shape=(training_config["IMAGE_SIZE"], training_config["IMAGE_SIZE"], 3)))
-  model.add(Conv2D(filters=256, kernel_size=6, activation='relu'))
-  model.add(Conv2D(filters=128, kernel_size=5, activation='relu'))
-  model.add(Conv2D(filters=16, kernel_size=3, activation='relu'))
-  model.add(Flatten())
-  model.add(Dense(1, activation='sigmoid'))
-  return model
-
-def SimplerModel(training_config):
-  model = Sequential(name="SimplerModel")
-  model.add(Input(shape=(training_config["IMAGE_SIZE"], training_config["IMAGE_SIZE"], 3)))
-  model.add(Conv2D(filters=128, kernel_size=6, activation='relu'))
+  model.add(Conv2D(filters=64, kernel_size=5, activation='relu'))
   model.add(MaxPool2D(2))
   model.add(Conv2D(filters=32, kernel_size=5, activation='relu'))
   model.add(MaxPool2D(2))
-  model.add(Conv2D(filters=16, kernel_size=3, activation='relu'))
+  model.add(Conv2D(filters=16, kernel_size=5, activation='relu'))
+  model.add(Dropout(0.5))
   model.add(Flatten())
   model.add(Dense(1, activation='sigmoid'))
   return model
