5780d60af2f8f136e446fb185bd4b8b85660dcd3
Tomas Mūžas <m_tomas@inbox.lt>
2024-01-15 21:22:22 +0200

    Fix

--- Augmentations/common_code/training_loop.py
+++ Augmentations/common_code/training_loop.py
@@ -288,7 +288,7 @@ def perform_training(models, training_config):
                 
                 wandb.finish()
 
-            tf.tpu.experimental.initialize_tpu_system(tf.distribute.cluster_resolver.TPUClusterResolver)
+            tf.tpu.experimental.initialize_tpu_system(tf.distribute.cluster_resolver.TPUClusterResolver())
             gc.collect()
             # tf.keras.backend.clear_session()
             # del cached_initial_training_dataset
