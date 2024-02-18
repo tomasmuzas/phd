0a7ae06eb3b5e3b1abe6d8b068a752bcd60729e3
Tomas Mūžas <m_tomas@inbox.lt>
2024-02-18 15:26:27 +0200

    Restore to default connection

--- Augmentations/common_code/utils.py
+++ Augmentations/common_code/utils.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 def reset_tpu(training_config):
   if training_config["TPU"]:
-    cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver.connect(tpu = 'node-1', zone = 'europe-west4-a', project = 'master2021-313119')
+    cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
     strategy = tf.distribute.TPUStrategy(cluster_resolver)
     print("Running on TPU ", cluster_resolver.master())
     print("REPLICAS: ", strategy.num_replicas_in_sync)
