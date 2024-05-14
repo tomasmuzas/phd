a824e12f62bd1a43a19727a29f763360efb0e8be
Tomas Mūžas <m_tomas@inbox.lt>
2024-05-14 20:53:46 +0300

    old way

--- Augmentations/common_code/utils.py
+++ Augmentations/common_code/utils.py
@@ -3,10 +3,9 @@ import tensorflow as tf
 def reset_tpu(training_config):
   if training_config["TPU"]:
     cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
-    tf.config.experimental_connect_to_cluster(cluster_resolver)
-    tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
     strategy = tf.distribute.TPUStrategy(cluster_resolver)
-    print(f'Running on a TPU w/{cluster_resolver.num_accelerators()["TPU"]} cores')
+    print("Running on TPU ", cluster_resolver.master())
+    print("REPLICAS: ", strategy.num_replicas_in_sync)
     return strategy
   else:
     gpus = tf.config.list_physical_devices('GPU')
