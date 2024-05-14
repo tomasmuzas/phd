acf52b5c3f2d3c9195da5a3585ec9e06787d1042
Tomas Mūžas <m_tomas@inbox.lt>
2024-05-14 21:01:08 +0300

    fix

--- Augmentations/common_code/utils.py
+++ Augmentations/common_code/utils.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 def reset_tpu(training_config):
   if training_config["TPU"]:
     cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
+    tf.config.experimental_connect_to_cluster(cluster_resolver)
     strategy = tf.distribute.TPUStrategy(cluster_resolver)
     print("Running on TPU ", cluster_resolver.master())
     print("REPLICAS: ", strategy.num_replicas_in_sync)
