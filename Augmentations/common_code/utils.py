832b0765fdafafaa3227e569c0baeebff47d2a75
Tomas Mūžas <m_tomas@inbox.lt>
2024-11-26 00:12:26 +0200

    Naming fix

--- Augmentations/common_code/utils.py
+++ Augmentations/common_code/utils.py
@@ -4,7 +4,7 @@ def reset_tpu(training_config):
   if training_config["TPU"]:
     cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
     tf.config.experimental_connect_to_cluster(cluster_resolver)
-    tf.tpu.experimental.initialize_tpu_system(tpu)
+    tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
     strategy = tf.distribute.TPUStrategy(cluster_resolver)
     print("Running on TPU ", cluster_resolver.master())
     print("REPLICAS: ", strategy.num_replicas_in_sync)
