148c4fd9fd8d4bcee383472ac86c1de079642d12
Tomas Mūžas <m_tomas@inbox.lt>
2024-11-26 00:10:59 +0200

    Initialize TPU system

--- Augmentations/common_code/utils.py
+++ Augmentations/common_code/utils.py
@@ -4,6 +4,7 @@ def reset_tpu(training_config):
   if training_config["TPU"]:
     cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
     tf.config.experimental_connect_to_cluster(cluster_resolver)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
     strategy = tf.distribute.TPUStrategy(cluster_resolver)
     print("Running on TPU ", cluster_resolver.master())
     print("REPLICAS: ", strategy.num_replicas_in_sync)
